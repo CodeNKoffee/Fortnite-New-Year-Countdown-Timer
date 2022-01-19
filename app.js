85482caa79e5f2ba18b7bf64e7ccd0cd5f10a525
Hatem Soliman <hatemthedev@gmail.com>
2022-01-19 22:24:42 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+// Update Countdown Time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = d < 10 ? "0" + d : d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
